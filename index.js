913c9e20da29d701e54f092da789c8b6e155a1bf
PuzzlingGGG <138084889+PuzzlingGGG@users.noreply.github.com>
2025-06-05 22:41:44 -0400

    make this better

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 8080 });
+const PORT = process.env.PORT || 8080;
+const wss = new WebSocket.Server({ port: PORT });
 
 const rooms = {};
 const clientLastMsg = new Map();
